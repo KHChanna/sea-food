c20dae61e1e1dc65b621265d80af3ab99323bfd2
Soben <ksoben.student@puthisastra.edu.kh>
2020-09-06 15:10:49 +0700

    merge channa branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,15 +65,6 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-<<<<<<< HEAD
-                <div class="top-right links">
-                    <a href="{{ url('/admin/') }}">Admin</a>
-                </div>
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-=======
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -91,7 +82,6 @@
             <div class="content">
                 <div class="title m-b-md">
                   Welcome
->>>>>>> d918ee12449ea9d9a0b7201d180c0b7ccce9b028
                 </div>
 
                 <div class="links">
